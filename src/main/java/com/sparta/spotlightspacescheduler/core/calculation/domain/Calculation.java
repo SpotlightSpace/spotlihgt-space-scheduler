328f7db39d7b6901d44b903825aeb3a78e10f8d4
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-11-19 05:28:17 +0900

    feat : 4차 구현 - JpaPagingItemReader사용 및 쿼리 최적화

--- src/main/java/com/sparta/spotlightspacescheduler/core/calculation/domain/Calculation.java
+++ src/main/java/com/sparta/spotlightspacescheduler/core/calculation/domain/Calculation.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,7 +22,7 @@ import lombok.NoArgsConstructor;
 public class Calculation extends Timestamped {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "calculation_id")
     private Long id;
 
--- src/main/java/com/sparta/spotlightspacescheduler/core/calculation/service/CalculationBatch.java
+++ src/main/java/com/sparta/spotlightspacescheduler/core/calculation/service/CalculationBatch.java
@@ -1,23 +1,18 @@
 package com.sparta.spotlightspacescheduler.core.calculation.service;
 
-import static com.sparta.spotlightspacescheduler.core.payment.domain.PaymentStatus.APPROVED;
-
-import com.sparta.spotlightspacescheduler.core.calculation.domain.Calculation;
 import com.sparta.spotlightspacescheduler.core.calculation.dto.CalculationProcessResponseDto;
-import com.sparta.spotlightspacescheduler.core.calculation.repository.CalculationRepository;
 import com.sparta.spotlightspacescheduler.core.event.domain.Event;
 import com.sparta.spotlightspacescheduler.core.payment.domain.Payment;
 import com.sparta.spotlightspacescheduler.core.payment.domain.PaymentStatus;
-import com.sparta.spotlightspacescheduler.core.payment.repository.PaymentRepository;
 import com.sparta.spotlightspacescheduler.core.point.point.domain.Point;
 import com.sparta.spotlightspacescheduler.core.point.point.repository.PointRepository;
 import com.sparta.spotlightspacescheduler.core.user.domain.User;
-
 import jakarta.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -35,6 +30,7 @@ import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.TransientDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.retry.RetryPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -46,10 +42,9 @@ public class CalculationBatch {
 
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
-    private final CalculationRepository calculationRepository;
-    private final PaymentRepository paymentRepository;
     private final PointRepository pointRepository;
     private final EntityManagerFactory entityManagerFactory;
+    private final JdbcTemplate jdbcTemplate;
 
     // 배치의 실행 단위 - 정산 배치
     @Bean
@@ -109,33 +104,66 @@ public class CalculationBatch {
     @Bean
     @StepScope
     public ItemProcessor<Payment, CalculationProcessResponseDto> paymentProcessor() {
+        Map<Long, Point> pointsCache = new HashMap<>(); // 캐시
+
         return new ItemProcessor<Payment, CalculationProcessResponseDto>() {
             @Override
             public CalculationProcessResponseDto process(Payment payment) throws Exception {
+                if (pointsCache.isEmpty()) {
+
+                    // 포인트 n+1문제 개선
+                    List<Point> points = pointRepository.findAll();
+                    for (Point point : points) {
+                        pointsCache.put(point.getUser().getId(), point);
+                    }
+                }
+
                 Event event = payment.getEvent();
                 User user = event.getUser();
-                Point point = pointRepository.findByUserId(user.getId());
+                Point point = pointsCache.get(user.getId());
                 return CalculationProcessResponseDto.of(user, point, event, payment.getOriginalAmount());
             }
         };
     }
+
 
     // 가공된 데이터를 저장하는 역할을 수행
     @Bean
     @StepScope
     public ItemWriter<CalculationProcessResponseDto> paymentWriter() {
         return new ItemWriter<CalculationProcessResponseDto>() {
+
             @Override
             public void write(Chunk<? extends CalculationProcessResponseDto> chunk) throws Exception {
+
+                String calculationSql = "INSERT INTO calculations (user_id, calculation_amount, create_at, update_at) VALUES (?, ?, ?, ?)";
+                List<Object[]> calculationBatchArgs = new ArrayList<>();
+
+                String pointUpdateSql = "UPDATE points SET amount = amount + ? WHERE user_id = ?";
+                List<Object[]> pointBatchArgs = new ArrayList<>();
+
+                //벌크 연산 로직
                 for (CalculationProcessResponseDto result : chunk.getItems()) {
+                    calculationBatchArgs.add(new Object[]{
+                            result.getUser().getId(),
+                            result.getOriginAmount(),
+                            LocalDateTime.now(),
+                            LocalDateTime.now()
+                    });
+
                     Point point = result.getPoint();
-                    Event event = result.getEvent();
                     point.addPoint(result.getOriginAmount());
+                    pointBatchArgs.add(new Object[]{
+                            result.getOriginAmount(),
+                            point.getUser().getId()
+                    });
+
+                    Event event = result.getEvent();
                     event.calculate();
-                    Calculation calculation = Calculation.create(result.getUser(), result.getOriginAmount());
-                    calculationRepository.save(calculation);
-                    pointRepository.save(point);
                 }
+
+                jdbcTemplate.batchUpdate(calculationSql, calculationBatchArgs);
+                jdbcTemplate.batchUpdate(pointUpdateSql, pointBatchArgs);
             }
         };
     }
@@ -144,7 +172,7 @@ public class CalculationBatch {
     @Bean
     public RetryPolicy retryPolicy() {
         Map<Class<? extends Throwable>, Boolean> exceptionClass = new HashMap<>();
-        exceptionClass.put(TransientDataAccessException.class, true);
+        exceptionClass.put(Exception.class, true);
         return new SimpleRetryPolicy(5, exceptionClass);
     }
 }
